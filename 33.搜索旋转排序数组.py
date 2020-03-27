709469103bbba4eb7340a84470f8dccb1cec6182
ChengYunfei <chengyunfei@megvii.com>
2020-03-27 17:37:33 +0800

    2020-03-27

---
+++ 33.搜索旋转排序数组.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=33 lang=python3
+#
+# [33] 搜索旋转排序数组
+#
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        try:
+            return nums.index(target)
+        except:
+            return -1
+# @lc code=end
+
